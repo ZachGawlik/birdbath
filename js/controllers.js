7fa1a3e7413859335adea837942b1699caba777e
Zach Gawlik <zachgawlik@gmail.com>
2015-07-03 21:59:29 -0700

    Change to rapid delete interface

--- js/controllers.js
+++ js/controllers.js
@@ -3,19 +3,35 @@ var birdbathControllers = angular.module('birdbathControllers', []);
 birdbathControllers.controller('TweetCtrl', function($http) {
     var ctrl = this;
     this.tweets = [];
-    this.deleted = [];
-    this.current = 0;
+    this.deletedCount = 0;
+    this.index = 0;
+    this.user = '';
 
+    // Initial first load
     $http.get('example.json').success(function(data) {
         ctrl.tweets = data;
+        ctrl.user = ctrl.tweets[0].user;
+        ctrl.nextTweet();
     });
 
     this.delete = function() {
         console.log('fakedelete');
+        this.deletedCount += 1;
+        this.nextTweet();
     };
 
     this.keep = function() {
         console.log('lolkeep');
+        this.nextTweet();
+    };
+
+    this.nextTweet = function() {
+        this.index += 1;
+        if (this.index >= this.tweets.length)
+            return;
+
+        this.tweet = this.tweets[this.index];
+        this.author = this.tweet.retweeted ? this.tweet.retweeted_status.user : this.tweet.user;
     };
 
     this.keyPress = function(e) {
@@ -29,5 +45,5 @@ birdbathControllers.controller('TweetCtrl', function($http) {
     this.softDeleteTweet = function(tweet) {
         tweet.isActive = false;
         this.deleted.push(tweet);
-    }
+    };
 });
